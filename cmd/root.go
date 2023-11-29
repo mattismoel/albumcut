403fd93df57fcb8d468c1ca2e19a43b0c24205dc
Mattis Kristensen <Mattis Ml Kristensen>
2023-11-29 16:32:39 +0100

    update description

--- cmd/root.go
+++ cmd/root.go
@@ -37,13 +37,7 @@ var (
 
 var rootCmd = &cobra.Command{
 	Use:   "albumcut",
-	Short: "Cut audio files given an input CSV file.",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A tool specifically designed for downloading live concerts or similar from YouTube and chopping them into an album format with individual tracks.",
 	Run: func(cmd *cobra.Command, args []string) {
 		outputDir = strings.TrimSuffix(outputDir, "/")
 
